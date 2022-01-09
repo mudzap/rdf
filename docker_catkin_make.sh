10317c87487ebeb8ac49b2b7bd9bd21f2c0d8f18
Iván Peña <ivan@corebooks.com>
2022-01-09 14:41:03 -0600

    Gazebo, programacion simple...

--- docker_catkin_make.sh
+++
@@ -1,6 +0,0 @@
-. ./common
-echo "Building catkin packages with container: ${RDF_CONTAINER_TAG}"
-docker run -v $(pwd)/catkin_ws:/root/catkin_ws --rm -it ${RDF_CONTAINER_TAG} bin/bash -c "\
-cd /root/catkin_ws; \
-catkin_make; \
-"
