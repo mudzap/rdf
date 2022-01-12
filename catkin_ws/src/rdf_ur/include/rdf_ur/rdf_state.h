83e668001cab85e8a16a458ffefa8f713317a112
Iván Peña <ivan@corebooks.com>
2022-01-11 22:08:43 -0600

    Abstraccion de joint_states y tf para usuario

--- catkin_ws/src/rdf_ur/include/rdf_ur/rdf_state.h
+++ catkin_ws/src/rdf_ur/include/rdf_ur/rdf_state.h
@@ -2,17 +2,22 @@
 
 #include <unordered_map>
 #include <string>
+#include <functional>
 
 #include <tf2_ros/transform_listener.h>
-
+#include <tf2_msgs/TFMessage.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <sensor_msgs/JointState.h>
 #include <std_msgs/Header.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Float64.h>
 
 #include <ros/ros.h>
+#include <ros/master.h>
 
 #include <Eigen/Dense.h>
+#include <Eigen/Geometry.h>
 
 /* Digital twins
  *
@@ -23,9 +28,10 @@
  *  If physical robot is active: Acquire robot state
  *  
  *  To differentiate sim from physical robot:
- *  - /ur_hardware_interface/joints exists (or /ur_hardware_interface for that matter)
+ *  - /ur_hardware_interface exists
  *  - /ur_hardware_interface/robot_mode > -1?
  *  - Is it possible to implement for all robots universally?
+ *  - /ur_hardware_interface name space can be edited in launch file (see: ur_control.launch)
  * 
  *  URDF: Contained in parameter /robot_description (nice!)
  *  - joint_state_publisher reads this parameter, automagically
@@ -37,27 +43,37 @@
 
 namespace rdf {
 
-   // Abstracts sensor_msgs::JointState (SoA -> AoS)
+   // Abstracts sensor_msgs::JointState (SoA -> AoS), utilized with map
    struct joint_state {
-      Eigen::Array3f position;
-      Eigen::Array3f velocity;
-      Eigen::Array3f effort;
+      double position;
+      double velocity;
+      double effort;
+   };
+
+   // Abstracts tf2_msgs::TFMessage to utilize with map
+   struct joint_transform {
+      Eigen::Vector3d position;
+      Eigen::Quaternion velocity;
    };
 
    class state {
         
          public:
-            state(int refresh_rate_hint = -1);
-            virtual std::unordered_map<std::string, joint_state> get_robot_state(); // Returns map of joint states
+            state(const int refresh_rate_hint = -1);
+            virtual std::unordered_map<std::string, joint_state> get_robot_joint_state(); // Returns map of joint states
             virtual joint_state get_joint_state(const std::string& j_name);         // Returns joint state (just use map!)
+            virtual std::unordered_map<std::string, joint_state> get_robot_transforms();
+            virtual joint_state get_joint_transform(const std::string& j_name);         
 
          private:
             bool is_sim;
+            
 
             virtual bool is_robot_simulated();
-            virtual void set_robot_state_pubs();
+            void setup_state_subscribers();
 
-            std::unordered_map<std::string, joint_state> joint_states // Not critical, maybe it is alright to use string?
+            std::unordered_map<std::string, joint_state> joint_states; // Not critical, maybe it is alright to use string?
+            std::unordered_map<std::string, joint_transform> joint_transforms;
             tf2_ros::Buffer tfBuffer;
             tf2_ros::TransformListener tfListener(tfBuffer);
 
--- catkin_ws/src/rdf_ur/src/rdf_state.cpp
+++ catkin_ws/src/rdf_ur/src/rdf_state.cpp
@@ -2,17 +2,48 @@
 
 namespace rdf {
 
-    state::state();
-        
-        public:
-            state();
+    state::state(const int refresh_rate_hint = -1) {
+        is_sim = is_robot_simulated();
+        setup_state_subscribers()
+    }
 
-        private:
-            bool is_sim;
+    void state::setup_state_subscribers(const int refresh_rate_hint = -1) {
+        /* Options:
+         * - /tf: Can be namespaced in launch file (for multiple robots)
+         * - /joint_states
+         */
+        ros::Subscriber sub = n.subscribe("/tf", 3,
+            [this](tf2_msgs::TFMessage tf2){
+                for (auto& tf_stamped : tf2) {
+                    const auto& tf = tf_stamped.transform;
+                    const auto& name = tf_stamped.header.frame_id;
+                    
+                    Eigen::Vector3d pos;
+                    Eigen::Quaternion quat;  
+                    tf2::fromMsg (tf.translation, pos)
+                    tf2::fromMsg (tf.rotation, quat)
 
-            virtual bool is_robot_simulated();
-            virtual void set_robot_state_pubs();
+                    const joint_transform j_tf = {tf.position, tf.rotation};
+                    this->joint_transforms[name] = j_tf;
+                }
+            });
+        ros::Subscriber sub = n.subscribe("/joint_states", 3,
+            [this](sensor_msgs::JointState js){
+                for (size_t it = 0; it < js.name.size(); it++) {
+                    this->joint_states[js.name[it]] = {js.position[it], js.velocity[it], js.effort[it]}
+                }
+            });
 
-    };
+    }
+
+    bool state::is_robot_simulated(const int refresh_rate_hint = -1) {
+        // Searches if any node contains "hardware_interface" in its name
+        ros::V_string nodes;
+        ros::master::getNodes(nodes);
+        for (auto &str: nodes) {
+            if(str.find("hardware_interface") != string::npos) {return false;}
+        }
+        return true;
+    }
     
 };
