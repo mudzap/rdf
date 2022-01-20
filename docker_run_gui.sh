2f3447a2aa7ede9890f5ae6c5ee919e2dbf7dbe3
Iván Peña <ivan@corebooks.com>
2022-01-20 08:48:58 -0600

    Ajustes directorios+

---
+++ docker_run_gui.sh
@@ -0,0 +1,37 @@
+. ./common
+
+echo "Running container: ${RDF_CONTAINER_TAG}_gui"
+
+xhost +local:docker
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+if [ ! -f $XAUTH ]
+then
+    xauth_list=$(xauth nlist :0 | sed -e 's/^..../ffff/')
+    if [ ! -z "$xauth_list" ]
+    then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+docker run -it \
+    	-v=$(pwd)/catkin_ws:/root/catkin_ws \
+    	-v=${HOME}/.ros:/root/.ros \
+    	-e="DISPLAY=$DISPLAY" \
+    	-e="QT_X11_NO_MITSHM=1" \
+    	-v="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    	-v="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    	-e="XAUTHORITY=$XAUTH" \
+    	-v="$XAUTH:$XAUTH" \
+    	--runtime=nvidia \
+    	--rm -it --net=host \
+	--privileged \
+    	${RDF_CONTAINER_TAG}_gui /bin/bash
+
+xhost -local:docker
